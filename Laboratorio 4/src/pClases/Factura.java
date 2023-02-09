cb1a66e58e52bdb8fec97e4715898a513ff09082
Leonardo Loaiza <leonardodario.loaiza@unmsm.edu.pe>
2023-02-09 15:05:32 -0500

    Clase factura

--- Laboratorio 4/src/pClases/Factura.java
+++ Laboratorio 4/src/pClases/Factura.java
@@ -10,4 +10,53 @@ package pClases;
  */
 public class Factura {
     
+    private int nFactura;
+    private String fecha;
+    private String vendedor;
+    private double monto;
+    
+    //Medoto constructor
+
+    public Factura(int nFactura, String fecha, String vendedor, double monto) {
+        this.nFactura = nFactura;
+        this.fecha = fecha;
+        this.vendedor = vendedor;
+        this.monto = monto;
+    }
+    
+    //Metodos set
+    public void setnFactura(int nFactura) {
+        this.nFactura = nFactura;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setVendedor(String vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+    
+    //Metodos get
+
+    public int getnFactura() {
+        return nFactura;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public String getVendedor() {
+        return vendedor;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+    
 }
